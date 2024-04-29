c5f89c0b96545e3f581db49b5164372d6091946f
Anderson Oliveira <thiagoaraujo.csm@gmail.com>
2024-04-29 10:59:34 -0300

    feat: Config JSP, Bootstrap, JQuery

--- src/main/java/com/andervilo/portifolio/controller/ProjetoController.java
+++ src/main/java/com/andervilo/portifolio/controller/ProjetoController.java
@@ -1,4 +1,15 @@
 package com.andervilo.portifolio.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
 public class ProjetoController {
+    @GetMapping("/")
+    public String home(Model model, HttpServletRequest httpServletRequest) {
+        return "index";
+    }
 }
--- src/main/java/com/andervilo/portifolio/model/entities/Membros.java
+++ src/main/java/com/andervilo/portifolio/model/entities/Membros.java
@@ -1,6 +1,7 @@
 package com.andervilo.portifolio.model.entities;
 
-import jakarta.persistence.*;
+
+import javax.persistence.*;
 
 @Entity
 @Table(name = "membros")
